5576e8bf7bab725b164eddfd3c64fd50c796a6d1
elmurie <elmurie@gmail.com>
2022-01-06 12:47:05 +0100

    added api call _draft_

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-import getProfile from './profile';
-import './style.css';
+import './style.scss';
+import axios from "axios";
 
-function draw() {
-    const el = document.createElement('div');
-    const btn = document.createElement('button');
-    el.innerHTML = 'Hello WebPack';
-    el.classList.add('hello');
-
-    btn.innerHTML = 'GET PROF';
-    btn.onclick = getProfile;
-    el.appendChild(btn);
-    return el;
+const planets = () => {
+    // https://www.swapi.dev/ is not working as of now ( 06/01/22, 11:52), so I am using a different API 
+    const axiosEndPoint = 'https://www.swapi.tech/api/planets'
+    const getData = async (url) => {
+        try {
+            const data = await axios.get(axiosEndPoint);
+            return data.data.results;
+        } catch (err) {
+            console.error('The Axios call has failed');
+        }
+    }
+    return getData(axiosEndPoint);
 }
 
-document.body.appendChild(draw());
+
+const main = document.querySelector('main')
+
+function extract() {
+    planets().then((output) => {
+        console.log(output);
+        let planetsArray = [];
+        output.forEach((element, index) => {
+            planetsArray.push(element);
+            let divPlanet = document.createElement("Div");
+            divPlanet.className = "planet";
+            divPlanet.innerHTML = element.name;
+            main.appendChild(divPlanet);
+            if ( index % 2 == 1 ) {
+                divPlanet.classList.add('right');
+            }
+        });
+        return planetsArray;
+    });
+}
+
+let dio = extract();
+console.log("test ", dio);
+
+
